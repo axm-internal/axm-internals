503032a63090451314482dd9250d4028b63d7467
Angel S. Moreno <angelxmoreno@gmail.com>
2026-01-25 15:40:29 -0500

    feat(repo-cli): integrated container and improved error handling

--- apps/repo-cli/src/cli.ts
+++ apps/repo-cli/src/cli.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env bun
+import 'reflect-metadata';
 import { CliApp } from '@axm-internal/cli-kit';
+import { ZodError } from 'zod';
+import { appContainer } from './appContainer';
 import { promptRunnerCommands } from './commands/prompt-runners';
+import { InteractiveOutputService } from './services/InteractiveOutputService';
 
+const interactiveOutputService = appContainer.resolve(InteractiveOutputService);
 const cliApp = new CliApp({
     config: {
         name: 'repo-cli',
@@ -9,9 +14,27 @@ const cliApp = new CliApp({
         version: '0.2.0',
     },
     options: {
+        container: appContainer,
         pretty: true,
         commandDefinitions: [...promptRunnerCommands],
+        onError: (error) => {
+            if (error instanceof ZodError) {
+                error.issues.forEach((issue) => {
+                    interactiveOutputService.logType({
+                        type: 'error',
+                        message: issue.message,
+                    });
+                });
+                return;
+            } else {
+                interactiveOutputService.logType({
+                    type: 'error',
+                    message: error.message,
+                });
+            }
+        },
     },
 });
+
 const exitCode = await cliApp.start();
 process.exit(exitCode);
