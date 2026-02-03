826760edf03406e7546799a0e65cc424b139443d
Angel S. Moreno <angelxmoreno@gmail.com>
2026-02-03 03:22:16 -0500

    chore: removed sample http-helper

--- monorepo-docs/http-helper/src/auth/index.ts
+++
@@ -1,9 +0,0 @@
-export { createBearerTokenChecker } from './createBearerTokenChecker';
-export { createCompositeTokenAuth } from './createCompositeTokenAuth';
-export { createQueryTokenChecker } from './createQueryTokenChecker';
-export type { TokenVerifier } from './TokenVerifier';
-export type {
-    CreateBearerTokenCheckerParams,
-    CreateCompositeTokenAuthParams,
-    CreateQueryTokenCheckerParams,
-} from './types';
--- monorepo-docs/http-helper/src/middleware/createRequestTrackingMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import { createMiddleware } from 'hono/factory';
-
-export const createRequestTrackingMiddleware = () =>
-    createMiddleware(async (c, next) => {
-        c.set('requestStartTime', Date.now());
-        await next();
-    });
